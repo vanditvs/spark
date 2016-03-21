2925ef8f21a435151848950668777258d1e48f9f
Vandit Sharma <1241vandit@gmail.com>
2016-03-22 00:05:59 +0530

    homepage route and controller

--- app/routes.php
+++ app/routes.php
@@ -1,22 +1,3 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
-Route::get('/', function()
-{
-	return "Hello";
-});
-
-Route::get('/user/{name?}', function($name = "user")
-{
-    return "Hello $name";
-});
+Route::get('/', array('as' => "index", 'uses' => "HomeController@showHomepage"));
