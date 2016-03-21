81fac391424f6b3820088d1ec4ca7afe334d55bb
Vandit Sharma <1241vandit@gmail.com>
2016-03-21 23:59:57 +0530

    create migrations for app schema.

---
+++ app/database/migrations/2016_03_21_143846_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('title');
+			$table->longText('content');
+			$table->string('slug')->unique();
+			$table->integer('blog_id')->unsigned();
+			$table->foreign('blog_id')->references('id')->on('blogs')->onDelete('cascade');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
