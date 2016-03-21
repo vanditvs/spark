0ebc5e743245eea5ea788452753ee3396ca1660a
Vandit Sharma <1241vandit@gmail.com>
2016-03-21 23:58:32 +0530

    local Database name change.

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,7 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
+			'database'  => 'spark',
 			'username'  => 'homestead',
 			'password'  => 'secret',
 			'charset'   => 'utf8',
