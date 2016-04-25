256dd14a32bff84fe4b50213a8a82cf6e69f7ce5
Vandit Sharma <1241vandit@gmail.com>
2016-04-25 12:57:30 +0530

    added admin in sidebar

--- app/views/partials/dashboard-sidebar.blade.php
+++ app/views/partials/dashboard-sidebar.blade.php
@@ -1,5 +1,9 @@
 <div class="col-lg-2 col-md-3 col-sm-3 col-xs-12 sidebar">
     <ul class="nav nav-sidebar text-right">
+        <li class="{{HTML::activeState('admin')}}">
+            <a href="{{route('admin')}}"><i class="glyphicon glyphicon-home"></i> Admin
+            </a>
+        </li>
         <li class="{{HTML::activeState('manage-blog', [$blog->id])}}">
             <a href="{{route('manage-blog', $blog->id)}}"><i class="glyphicon glyphicon-th-large"></i> Dashboard</a>
         </li>
